236105bab93808a7e806bca1f6d62ac9afdafdab
eminencegrs <a.dariienko@outlook.com>
2024-08-06 19:20:30 +0200

    Add the unit tests.

--- src/CSharp.Experiments.Algorithms/Sorting/MergeSort.cs
+++ src/CSharp.Experiments.Algorithms/Sorting/MergeSort.cs
@@ -1,31 +1,61 @@
 ﻿namespace CSharp.Experiments.Algorithms.Sorting;
 
+// It recursively divides the array into halves until each subarray contains a single element,
+// which is inherently sorted. Then, it merges the sorted subarrays back together to form a fully sorted array.
+//
+// Time Complexity: O(n * log(n)).
+//  ==> Divide Step: O(log(n)
+//  ==> Merge Step: O(n)
 public class MergeSort
 {
-    public void Sort(int[] numbers)
+    public void Run(int[] numbers)
     {
-        if (numbers.Length <= 1) { return; }
+        ArgumentNullException.ThrowIfNull(numbers, nameof(numbers));
 
-        int mid = numbers.Length / 2;
-        int[] left = GetSubarray(numbers, 0, mid - 1);
-        int[] right = GetSubarray(numbers, mid, numbers.Length - 1);
+        if (numbers.Length <= 1)
+        {
+            return;
+        }
 
-        this.Sort(left);
-        this.Sort(right);
+        var (left, right) = SplitInHalf(numbers);
 
-        int i = 0;
-        int j = 0;
-        int k = 0;
+        this.Run(left);
+        this.Run(right);
+
+        Merge(numbers, left, right);
+    }
+
+    private static (int[] left, int[] right) SplitInHalf(int[] numbers)
+    {
+        var mid = numbers.Length / 2;
+        var left = GetSubarray(numbers, from: 0, to: mid - 1);
+        var right = GetSubarray(numbers, from: mid, to: numbers.Length - 1);
+        return (left, right);
+    }
+
+    private static int[] GetSubarray(int[] array, int from, int to)
+    {
+        var length = to - from + 1;
+        var result = new int[length];
+        Array.Copy(array, from, result, 0, length);
+        return result;
+    }
+
+    private static void Merge(int[] array, int[] left, int[] right)
+    {
+        var i = 0;
+        var j = 0;
+        var k = 0;
 
         while (i < left.Length && j < right.Length)
         {
             if (left[i] <= right[j])
             {
-                numbers[k] = left[i++];
+                array[k] = left[i++];
             }
             else
             {
-                numbers[k] = right[j++];
+                array[k] = right[j++];
             }
 
             k++;
@@ -33,20 +63,12 @@ public class MergeSort
 
         while (i < left.Length)
         {
-            numbers[k++] = left[i++];
+            array[k++] = left[i++];
         }
 
         while (j < right.Length)
         {
-            numbers[k++] = right[j++];
+            array[k++] = right[j++];
         }
-    }
-
-    private static int[] GetSubarray(int[] numbers, int start, int end)
-    {
-        int length = end - start + 1;
-        int[] result = new int[length];
-        Array.Copy(numbers, start, result, 0, length);
-        return result;
     }
 }
--- tests/CSharp.Experiments.Algorithms.Tests/Sorting/MergeSortTests.cs
+++ tests/CSharp.Experiments.Algorithms.Tests/Sorting/MergeSortTests.cs
@@ -7,9 +7,9 @@ public class MergeSortTests
 {
     [Theory]
     [MemberData(nameof(TestData))]
-    public void GiveArray_WhenSort_ThenResultAsExpected(int[] numbers, int[] expectedResult)
+    public void GiveArray_WhenCallRun_ThenResultAsExpected(int[] numbers, int[] expectedResult)
     {
-        new MergeSort().Sort(numbers);
+        new MergeSort().Run(numbers);
         numbers.ShouldBeEquivalentTo(expectedResult);
     }
 
@@ -17,8 +17,38 @@ public class MergeSortTests
     {
         yield return
         [
-            new[] { -11, 12, -42, 0, 1, 90, 68, 6, -9 },
-            new[] { -42, -11, -9, 0, 1, 6, 12, 68, 90 }
+            new int[] { },
+            new int[] { }
+        ];
+        yield return
+        [
+            new[] { 5 },
+            new[] { 5 },
+        ];
+        yield return
+        [
+            new[] { 1, 2, 3, 4, 5 },
+            new[] { 1, 2, 3, 4, 5 },
+        ];
+        yield return
+        [
+            new[] { 5, 4, 3, 2, 1 },
+            new[] { 1, 2, 3, 4, 5 },
+        ];
+        yield return
+        [
+            new[] { 3, 1, -4, 2, 5, 9, -7, 6, 8, 0, -5 },
+            new[] { -7, -5, -4, 0, 1, 2, 3, 5, 6, 8, 9 }
+        ];
+        yield return
+        [
+            new[] { 5, 2, 3, 3, 2, 2, 1, 4, 4, 4, 5 },
+            new[] { 1, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5 }
+        ];
+        yield return
+        [
+            new[] { -3, -1, -4, -1, -5, -9, -2, -6, -5, -3, -5 },
+            new[] { -9, -6, -5, -5, -5, -4, -3, -3, -2, -1, -1 }
         ];
     }
 }
