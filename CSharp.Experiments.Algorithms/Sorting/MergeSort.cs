78cf86ec1b0fe6789c776df681f7881cfeac12fa
eminencegrs <a.dariienko@outlook.com>
2024-08-06 16:28:38 +0200

    Add a new project: CSharp.Experiments.Algorithms.

---
+++ CSharp.Experiments.Algorithms/Sorting/MergeSort.cs
@@ -0,0 +1,52 @@
+﻿namespace CSharp.Experiments.Algorithms.Sorting;
+
+public class MergeSort
+{
+    public void Sort(int[] numbers)
+    {
+        if (numbers.Length <= 1) { return; }
+
+        int mid = numbers.Length / 2;
+        int[] left = GetSubarray(numbers, 0, mid - 1);
+        int[] right = GetSubarray(numbers, mid, numbers.Length - 1);
+
+        this.Sort(left);
+        this.Sort(right);
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.Length && j < right.Length)
+        {
+            if (left[i] <= right[j])
+            {
+                numbers[k] = left[i++];
+            }
+            else
+            {
+                numbers[k] = right[j++];
+            }
+
+            k++;
+        }
+
+        while (i < left.Length)
+        {
+            numbers[k++] = left[i++];
+        }
+
+        while (j < right.Length)
+        {
+            numbers[k++] = right[j++];
+        }
+    }
+
+    private static int[] GetSubarray(int[] numbers, int start, int end)
+    {
+        int length = end - start + 1;
+        int[] result = new int[length];
+        Array.Copy(numbers, start, result, 0, length);
+        return result;
+    }
+}
---
+++ tests/CSharp.Experiments.Algorithms.Tests/Sorting/MergeSortTests.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel;
+using CSharp.Experiments.Algorithms.Sorting;
+using Shouldly;
+
+namespace CSharp.Experiments.Algorithms.Tests.Sorting;
+
+public class MergeSortTests
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void GiveArray_WhenCallMergeSort_ThenResultAsExpected(int[] numbers, int[] expectedResult)
+    {
+        new MergeSort().Sort(numbers);
+        numbers.ShouldBeEquivalentTo(expectedResult);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return
+        [
+            new[] { -11, 12, -42, 0, 1, 90, 68, 6, -9 },
+            new[] { -42, -11, -9, 0, 1, 6, 12, 68, 90 }
+        ];
+    }
+}
