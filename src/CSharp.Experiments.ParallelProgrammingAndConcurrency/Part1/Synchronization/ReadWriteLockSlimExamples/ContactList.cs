15ff3286719514e2e2f2673e91d1f05f3add706d
eminencegrs <a.dariienko@outlook.com>
2024-10-23 17:21:28 +0200

    Synchronization mechanisms: ReadWriteLockSlim.

---
+++ src/CSharp.Experiments.ParallelProgrammingAndConcurrency/Part1/Synchronization/ReadWriteLockSlimExamples/ContactList.cs
@@ -0,0 +1,45 @@
+namespace CSharp.Experiments.ParallelProgrammingAndConcurrency.Part1.Synchronization.ReadWriteLockSlimExamples;
+
+public class ContactList
+{
+    private readonly List<Contact> contacts;
+
+    private readonly ReaderWriterLockSlim contactLock = new();
+
+    public ContactList(List<Contact> initialContacts)
+    {
+        this.contacts = initialContacts;
+    }
+
+    public void AddContact(Contact newContact)
+    {
+        try
+        {
+            this.contactLock.EnterWriteLock();
+            this.contacts.Add(newContact);
+        }
+        finally
+        {
+            this.contactLock.ExitWriteLock();
+        }
+    }
+
+    public Contact? GetContactByPhoneNumber(string phoneNumber)
+    {
+        try
+        {
+            this.contactLock.EnterReadLock();
+            return this.contacts.FirstOrDefault(x => x.PhoneNumber == phoneNumber);
+        }
+        finally
+        {
+            this.contactLock.ExitReadLock();
+        }
+    }
+}
+
+public record Contact
+{
+    public string Name { get; init; } = Guid.NewGuid().ToString();
+    public string? PhoneNumber { get; init; }
+}
---
+++ tests/CSharp.Experiments.ParallelProgrammingAndConcurrency.UnitTests/Part1/Synchronization/ReadWriteLockSlimExamples/ContactListTests.cs
@@ -0,0 +1,100 @@
+using CSharp.Experiments.ParallelProgrammingAndConcurrency.Part1.Synchronization.ReadWriteLockSlimExamples;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Shouldly;
+using Xunit;
+
+namespace CSharp.Experiments.ParallelProgrammingAndConcurrency.UnitTests.Part1.Synchronization.ReadWriteLockSlimExamples;
+
+public class ContactListTests
+{
+    [Fact]
+    public void GivenMultipleThreads_WhenAddingContacts_ThenAllContactsShouldBeAdded()
+    {
+        var initialContacts = new List<Contact>();
+        var contactList = new ContactList(initialContacts);
+        var newContacts = new List<Contact>
+        {
+            new() { PhoneNumber = "12345" },
+            new() { PhoneNumber = "67890" },
+            new() { PhoneNumber = "54321" },
+            new() { PhoneNumber = "98765" }
+        };
+
+        var threads = new List<Thread>();
+        foreach (var contact in newContacts)
+        {
+            var thread = new Thread(() => contactList.AddContact(contact));
+            threads.Add(thread);
+            thread.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        using (new AssertionScope())
+        {
+            initialContacts.Count.ShouldBe(newContacts.Count);
+            initialContacts.Should().BeEquivalentTo(newContacts);
+        }
+    }
+
+    [Fact]
+    public void GivenMultipleThreads_WhenReadingContacts_ThenContactsShouldBeReturnedCorrectly()
+    {
+        var phoneNumberToFind = "12345";
+        var initialContacts = new List<Contact>
+        {
+            new() { PhoneNumber = "12345" },
+            new() { PhoneNumber = "67890" },
+            new() { PhoneNumber = "54321" }
+        };
+
+        var contactList = new ContactList(initialContacts);
+
+        Contact? foundContact = null;
+        var thread = new Thread(() => foundContact = contactList.GetContactByPhoneNumber(phoneNumberToFind));
+        thread.Start();
+        thread.Join();
+
+        using (new AssertionScope())
+        {
+            foundContact.Should().NotBeNull();
+            foundContact!.PhoneNumber.ShouldBe(phoneNumberToFind);
+        }
+    }
+
+    [Fact]
+    public void GivenMultipleThreads_WhenSimultaneousReadAndWrite_ThenReadsAreConsistent()
+    {
+        var phoneNumberToFind = "67890";
+        var initialContacts = new List<Contact>
+        {
+            new() { PhoneNumber = "12345" },
+            new() { PhoneNumber = "67890" }
+        };
+
+        var contactList = new ContactList(initialContacts);
+        var newContact = new Contact { PhoneNumber = "54321" };
+
+        var writeThread = new Thread(() => contactList.AddContact(newContact));
+
+        Contact? foundContact = null;
+        var readThread = new Thread(() => foundContact = contactList.GetContactByPhoneNumber(phoneNumberToFind));
+
+        writeThread.Start();
+        readThread.Start();
+
+        writeThread.Join();
+        readThread.Join();
+
+        using (new AssertionScope())
+        {
+            initialContacts.Should().Contain(newContact);
+            foundContact.Should().NotBeNull();
+            foundContact!.PhoneNumber.ShouldBe(phoneNumberToFind);
+        }
+    }
+}
